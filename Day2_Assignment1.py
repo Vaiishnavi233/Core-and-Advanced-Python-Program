2ecc04aa3a455e6bbaec1f970693e7fe6f927246
Vaiishnavi233 <mohiteshobha067@gmail.com>
2025-02-12 21:24:23 +0530

    Add files via upload

---
+++ Day2_Assignment1.py
@@ -0,0 +1,20 @@
+# Define two numbers
+num1 = 5
+num2 = 8
+
+# Calculate multiplication
+multiplication = num1 * num2
+
+# Calculate sum
+sum_result = num1 + num2
+
+# Print the results
+print("Multiplication of", num1, "and", num2, "is:", multiplication)
+print("Sum of", num1, "and", num2, "is:", sum_result)
+
+
+
+'''Output:
+ Multiplication of 5 and 8 is: 40
+Sum of 5 and 8 is: 13
+'''
